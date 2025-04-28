e2e2d734fb59026de6691e54db184dc914519af5
clrowex <clrowex@gmail.com>
2025-04-28 14:05:45 +0000

    Anykernel3 Update banner etc
    Signed-off-by: clrowex <clrowex@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by @DenomSly
+kernel.string=Kernel by @Clrowex 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=sweet
 device.name2=sweetin
-supported.versions=13 - 15
+supported.versions=11 - 15
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
